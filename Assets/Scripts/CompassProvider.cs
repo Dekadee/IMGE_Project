34047be852f12558fcbceb569f278e0de8850fc9
Dekadee <dominic.ebner@outlook.com>
2017-12-28 17:24:13 +0100

    Basic controls and simple backgrounds

---
+++ Assets/Scripts/CompassProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompassProvider : MonoBehaviour {
+
+    private float heading = 0;
+
+    private float oldHeading = 0;
+
+    private float speed = 0.5f;
+
+    [SerializeField]
+    Text text;
+
+    // Use this for initialization
+    void Start () {
+        Input.compass.enabled = true;
+        Input.location.Start();
+        Debug.Log("Compass initialized");
+        heading = -Input.compass.magneticHeading;
+        oldHeading = -Input.compass.magneticHeading;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        heading = -Input.compass.magneticHeading;
+
+        //heading = oldHeading + 0.8f * (heading - oldHeading);
+
+        //oldHeading = heading;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.Euler(0, heading, 0),speed );
+
+        text.text = heading.ToString();
+    }
+
+    public float GetHeading()
+    {
+        return heading;
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    Transform player;
+    
+	// Use this for initialization
+	void Start () {
+
+        player = gameObject.GetComponent<Transform>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        #region WASD Movement
+
+        if (Input.GetKey("w"))
+        {
+            player.Translate(new Vector3(1, 0, 0));
+        }
+        if (Input.GetKey("s"))
+        {
+            player.Translate(new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKey("a"))
+        {
+            player.Translate(new Vector3(0, 0, 1));
+        }
+        if (Input.GetKey("d"))
+        {
+            player.Translate(new Vector3(0, 0, -1));
+        }
+        #endregion
+
+        transform.Translate(10*Time.deltaTime, 0, 0);
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
